c7d0cc8c153aff5427a922c2bae354afa5f19892
eziquexx <waftyann@gmail.com>
2025-08-07 16:25:30 +0900

    refactor(adminbook) [backend]: 도서관련 에러, 성공 메시지 리팩토링

--- backend/src/main/java/com/jelee/librarymanagementsystem/global/enums/ErrorCode.java
+++ backend/src/main/java/com/jelee/librarymanagementsystem/global/enums/ErrorCode.java
@@ -12,7 +12,20 @@ public enum ErrorCode {
   FORBIDDEN(HttpStatus.FORBIDDEN, "AUTH403", "error.forbidden"),
   USER_EMAIL_SAME(HttpStatus.BAD_REQUEST, "USER_003", "error.email.same"),
   USER_PASSWORD_SAME(HttpStatus.BAD_REQUEST, "USER_004", "error.password.same"),
-  USER_PASSWORD_NOTSAME(HttpStatus.BAD_REQUEST, "USER_005", "error.password.notsame");
+  USER_PASSWORD_NOTSAME(HttpStatus.BAD_REQUEST, "USER_005", "error.password.notsame"),
+  //400
+  BOOK_TITLE_BLANK(HttpStatus.BAD_REQUEST, "BOOK_001", "error.book.title.blank"),
+  BOOK_AUTHOR_BLANK(HttpStatus.BAD_REQUEST, "BOOK_002", "error.book.author.blank"),
+  BOOK_DATE_INVALID(HttpStatus.BAD_REQUEST, "BOOK_004", "error.book.date.invalid"),
+
+  // 404
+  BOOK_NOT_FOUND(HttpStatus.NOT_FOUND, "BOOK_404", "error.book.not_found"),
+
+  // 409
+  BOOK_TITLE_DUPLICATED(HttpStatus.CONFLICT, "BOOK_409", "error.book.title.duplicate"),
+
+  // 500
+  BOOK_UPDATE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "BOOK_500", "error.book.update_failed");
 
   private final HttpStatus httpStatus;
   private final String code;
--- backend/src/main/java/com/jelee/librarymanagementsystem/global/enums/SuccessCode.java
+++ backend/src/main/java/com/jelee/librarymanagementsystem/global/enums/SuccessCode.java
@@ -11,7 +11,12 @@ public enum SuccessCode {
   BOOK_REGISTERED(HttpStatus.CREATED, "BOOK_201", "success.book.registered"),
   USER_EMAIL_UPDATE_SUCCESS(HttpStatus.OK, "AUTH_200", "success.user.email.update"),
   USER_PASSWORD_UPDATE_SUCCESS(HttpStatus.OK, "AUTH_200", "success.user.password.update"),
-  USER_DELETE_ACCOUNT_SUCCESS(HttpStatus.OK, "AUTH_200", "success.user.delete.account");
+  USER_DELETE_ACCOUNT_SUCCESS(HttpStatus.OK, "AUTH_200", "success.user.delete.account"),
+  BOOK_CREATED(HttpStatus.CREATED, "BOOK_201", "도서가 성공적으로 등록되었습니다."),
+  BOOK_UPDATED(HttpStatus.OK, "BOOK_200", "도서 수정이 완료되었습니다."),
+  BOOK_DELETED(HttpStatus.OK, "BOOK_204", "도서가 삭제되었습니다."),
+  BOOK_FETCHED(HttpStatus.OK, "BOOK_210", "도서 조회가 완료되었습니다."),
+  BOOK_LIST_FETCHED(HttpStatus.OK, "BOOK_211", "도서 목록 조회가 완료되었습니다.");
  
   // 필드
   private final HttpStatus httpStatus;
