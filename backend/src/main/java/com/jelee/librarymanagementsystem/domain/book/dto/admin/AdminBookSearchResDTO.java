24cba65b3941089533628f09a5e1f23b6c133760
eziquexx <waftyann@gmail.com>
2025-08-31 00:14:21 +0900

    refactor [backend]: 관리자-도서관리 전체목록조회, 검색조회시 응답받는 DTO 필드 수정

--- backend/src/main/java/com/jelee/librarymanagementsystem/domain/book/dto/admin/AdminBookSearchResDTO.java
+++ backend/src/main/java/com/jelee/librarymanagementsystem/domain/book/dto/admin/AdminBookSearchResDTO.java
@@ -1,8 +1,10 @@
 package com.jelee.librarymanagementsystem.domain.book.dto.admin;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import com.jelee.librarymanagementsystem.domain.book.entity.Book;
+import com.jelee.librarymanagementsystem.domain.book.enums.BookStatus;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,17 +16,25 @@ import lombok.NoArgsConstructor;
 public class AdminBookSearchResDTO {
   private Long id;
   private String title;
+  private String isbn;
   private String author;
   private String publisher;
   private LocalDate publishedDate;
+  private BookStatus status;
   private String location;
+  private LocalDateTime createdAt;
+  private LocalDateTime updatedAt;
 
   public AdminBookSearchResDTO(Book book) {
     this.id = book.getId();
     this.title = book.getTitle();
+    this.isbn = book.getIsbn();
     this.author = book.getAuthor();
     this.publisher = book.getPublisher();
     this.publishedDate = book.getPublishedDate();
+    this.status = book.getStatus();
     this.location = book.getLocation();
+    this.createdAt = book.getCreatedAt();
+    this.updatedAt = book.getUpdatedAt();
   }
 }
