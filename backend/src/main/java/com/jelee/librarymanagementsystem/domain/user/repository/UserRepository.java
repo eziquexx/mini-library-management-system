43f39778d9c3aa93f838b7c64ca48d100e524869
eziquexx <waftyann@gmail.com>
2025-09-12 15:36:27 +0900

    refactor(user_admin) [backend]: Jpa Repository를 상속받기 때문에 Repository에서 findAll 메서드 제거

--- backend/src/main/java/com/jelee/librarymanagementsystem/domain/user/repository/UserRepository.java
+++ backend/src/main/java/com/jelee/librarymanagementsystem/domain/user/repository/UserRepository.java
@@ -7,7 +7,6 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.lang.NonNull;
 
 import com.jelee.librarymanagementsystem.domain.user.entity.User;
 import com.jelee.librarymanagementsystem.global.enums.UserStatus;
@@ -23,8 +22,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
   Optional<User> findByUsername(String username);
 
   // 관리자 - 사용자 전체 목록 조회 (+페이징)
-  @NonNull
-  Page<User> findAll(Pageable pageable);
+  // Page<User> findAll(Pageable pageable);
 
   // 관리자 - 회원 username 검색 (+페이징)
   Page<User> findByUsernameContainingIgnoreCase(String username, Pageable pageable);
