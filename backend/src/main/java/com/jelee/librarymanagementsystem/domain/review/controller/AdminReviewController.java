2e68cfc7eacf6f6a86a7e7b00aa7ee88eea8d50d
eziquexx <waftyann@gmail.com>
2025-09-22 17:15:29 +0900

    feat(review_admin) [backend]: 관리자 리뷰 전체 목록 조회 기능 추가

---
+++ backend/src/main/java/com/jelee/librarymanagementsystem/domain/review/controller/AdminReviewController.java
@@ -0,0 +1,49 @@
+package com.jelee.librarymanagementsystem.domain.review.controller;
+
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jelee.librarymanagementsystem.domain.review.dto.admin.AdminReviewListResDTO;
+import com.jelee.librarymanagementsystem.domain.review.service.AdminReviewService;
+import com.jelee.librarymanagementsystem.domain.user.entity.User;
+import com.jelee.librarymanagementsystem.global.response.ApiResponse;
+import com.jelee.librarymanagementsystem.global.response.code.ReviewSuccessCode;
+import com.jelee.librarymanagementsystem.global.util.MessageProvider;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/admin")
+@RequiredArgsConstructor
+public class AdminReviewController {
+  
+  private final AdminReviewService adminReviewService;
+  private final MessageProvider messageProvider;
+
+  // 관리자: 책 리뷰 전체 목록
+  @GetMapping("/reviews")
+  public ResponseEntity<?> allListReview(
+    @RequestParam(name = "page", defaultValue = "0") int page,
+    @RequestParam(name = "size", defaultValue = "10") int size,
+    @AuthenticationPrincipal User user) {
+    
+      // 서비스로직
+      Page<AdminReviewListResDTO> responseDTO = adminReviewService.allListReview(page, size, user.getId());
+
+      // 성공메시지
+      String message = messageProvider.getMessage(ReviewSuccessCode.REVIEW_LIST_FETCHED.getMessage());
+    
+      // 반환
+      return ResponseEntity
+                .status(ReviewSuccessCode.REVIEW_LIST_FETCHED.getHttpStatus())
+                .body(ApiResponse.success(
+                  ReviewSuccessCode.REVIEW_LIST_FETCHED, 
+                  message, 
+                  responseDTO));
+  }
+}
