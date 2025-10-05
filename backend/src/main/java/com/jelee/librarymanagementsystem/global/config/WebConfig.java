f6e701459b55f158bbb41f7e13d74345a158b442
eziquexx <waftyann@gmail.com>
2025-10-05 23:16:10 +0900

    feat(cors/config) [backend]: CORS 설정 WebConfig 추가

---
+++ backend/src/main/java/com/jelee/librarymanagementsystem/global/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.jelee.librarymanagementsystem.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+  
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**") // 모든 경로에 대해
+            .allowedOrigins("http://localhost:3000") // React 앱 주소 허용
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // 허용 HTTP 메서드
+            .allowCredentials(true); // 쿠키 허용 등 인증 관련 설정
+  }
+}
