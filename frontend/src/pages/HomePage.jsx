af7a15fd514532d3c06a02597d980597ef35ecc1
eziquexx <waftyann@gmail.com>
2025-10-07 21:28:18 +0900

    feat(home) [frontend]: 로그인 기능 구현 및 UI 수정

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,69 +1,131 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 const HomePage = () => {
 
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get("http://localhost:8080/api/v1/user/me", 
+          { withCredentials: true,
+            headers: {
+              Accept: "application/json",
+            },
+          });
+          setUser(response.data.data.username);
+          console.log(response.data.data);
+      } catch(error) {
+        // 로그인 안된 상태 체크하여 user에 null값 유지
+        if (error.response && error.response.status === 401) {
+          setUser(null);
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  if (loading) {
+    return <div>로딩 중...</div>
+  }
 
   const goToLoginPage = () => {
     navigate("/login");
   }
 
+  // 로그아웃
+  const goToLogout = async () => {
+    await axios.post(
+      "http://localhost:8080/api/v1/auth/logout",
+      null,
+      { withCredentials: true }
+    );
+    setUser(null);
+  }
+
   return (
     <>
       <div className="w-full">
-        <header className="w-full h-auto flex flex-col">
-          <div className="w-full h-10 flex flex-row justify-between lg:px-12 px-7 leading-10 bg-teal-500 text-sm">
-            <h1>로고</h1>
-            <div className="flex flex-row">
-              <Link to="/login">로그인</Link>
-              <span className="mr-2">님</span>
-              <div>마이페이지</div>
+        <header className="flex flex-col">
+          {/* header1 */}
+          <div className="w-full flex justify-center bg-teal-500">
+            <div className="w-7xl max-w-7xl h-10 flex flex-row justify-between px-7 lg:px-12  leading-10  text-sm">
+              <h1><Link to="/">로고</Link></h1>
+              <div className="flex flex-row">
+                {user ? (
+                  <>
+                    <span>{user}님</span>
+                    <div className="mx-2">|</div>
+                    <div className="cursor-pointer">마이페이지</div>
+                    <div className="mx-2">|</div>
+                    <div onClick={goToLogout} className="cursor-pointer">로그아웃</div>
+                  </>
+                ) : (
+                  <Link to="/login">로그인</Link>
+                )}
+                
+                {/* <Link to="/login">로그인</Link>
+                <span className="mr-2">님</span>
+                <div>마이페이지</div> */}
+              </div>
             </div>
           </div>
-          <div className="w-full h-14 lg:px-12 px-7 leading-14 bg-teal-400">
-            <ul className="flex flex-row">
-              <li className="mr-2">홈</li>
-              <li>공지사항</li>
-            </ul>
+          {/* header2 */}
+          <div className="w-full flex justify-center bg-teal-400">
+            <div className="w-7xl max-w-7xl h-14 px-7 lg:px-12 leading-14">
+              <ul className="flex flex-row">
+                <li className="mr-2"><Link to="/">홈</Link></li>
+                <li><Link to="/notice">공지사항</Link></li>
+              </ul>
+            </div>
           </div>
         </header>
-        <div className="w-full flex flex-col px-12">
-          <div className="flex flex-col mt-26 ">
-            <h1 className="text-3xl font-bold text-center">행복 도서관</h1>
-            <span className="text-center mt-5">행복 도서관에 오신 것을 환영합니다.</span>
-          </div>
-          <div className="flex justify-center mt-6">
-            <form action="">
-              <div className="flex flex-row">
-                <input 
-                  type="text" 
-                  placeholder="검색어를 입력해주세요"
-                  className="
-                    col-span-3 
-                    w-100
-                    py-3 px-3 
-                    border rounded-l-sm border-gray-300 
-                    text-sm leading-5 
-                    bg-white"
-                />
-                <button className="rounded-r-sm bg-teal-500 text-white px-5">검색</button>
-              </div>
-            </form>
-          </div>
-          <div className="border mt-7">
-            <div class="grid grid-cols-4 gap-4">
-              <div className="border p-14">01</div>
-              <div className="border p-14">02</div>
-              <div className="border p-14">03</div>
-              <div className="border p-14">04</div>
-              <div className="border p-14">05</div>
-              <div className="border p-14">06</div>
-              <div className="border p-14">07</div>
-              <div className="border p-14">08</div>
-              <div className="border p-14">09</div>
+        <div className="w-full flex justify-center px-7 lg:px-12">
+          <div className="w-7xl max-w-7xl flex flex-col overflow-hidden">
+            <div className="w-full flex flex-col mt-26 ">
+              <h1 className="text-3xl font-bold text-center">행복 도서관</h1>
+              <span className="text-center mt-5">행복 도서관에 오신 것을 환영합니다.</span>
             </div>
-          </div>
+            <div className="w-full flex justify-center mt-6">
+              <form action="">
+                <div className="flex flex-row">
+                  <input 
+                    type="text" 
+                    placeholder="검색어를 입력해주세요"
+                    className="
+                      col-span-3 
+                      w-100
+                      grow
+                      py-3 px-3 
+                      border rounded-l-sm border-gray-300 
+                      text-sm leading-5 
+                      bg-white"
+                  />
+                  <button className="rounded-r-sm bg-teal-500 text-white px-5 flex-none">검색</button>
+                </div>
+              </form>
+            </div>
+            <div className="border mt-7 overflow-hidden">
+              <div className="grid grid-cols-4 gap-4">
+                <div className="border p-14">01</div>
+                <div className="border p-14">02</div>
+                <div className="border p-14">03</div>
+                <div className="border p-14">04</div>
+                <div className="border p-14">05</div>
+                <div className="border p-14">06</div>
+                <div className="border p-14">07</div>
+                <div className="border p-14">08</div>
+                <div className="border p-14">09</div>
+              </div>
+            </div>
+        </div>
         </div>
       </div>
     </>
