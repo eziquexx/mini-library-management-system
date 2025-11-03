27e8470ea899aa53cafd1a6ef78e238c769de595
eziquexx <waftyann@gmail.com>
2025-11-03 17:22:20 +0900

    feat(reivew) [backend]: 사용자 리뷰 전체 목록과 리뷰 상세 조회 시 응답 DTO에 필요한 필드 추가/수정

--- backend/src/main/java/com/jelee/librarymanagementsystem/domain/review/dto/user/UserReviewDetailResDTO.java
+++ backend/src/main/java/com/jelee/librarymanagementsystem/domain/review/dto/user/UserReviewDetailResDTO.java
@@ -1,5 +1,6 @@
 package com.jelee.librarymanagementsystem.domain.review.dto.user;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import com.jelee.librarymanagementsystem.domain.review.entity.Review;
@@ -8,17 +9,23 @@ import lombok.Getter;
 
 @Getter
 public class UserReviewDetailResDTO {
+  private Long id;
   private String bookTitle;
   private String author;
   private String publisher;
+  private LocalDate publishedDate;
+  private String isbn;
   private String content;
   private LocalDateTime createdDate;
   private LocalDateTime updatedDate;
 
   public UserReviewDetailResDTO(Review review) {
+    this.id = review.getId();
     this.bookTitle = review.getBook().getTitle();
     this.author = review.getBook().getAuthor();
     this.publisher = review.getBook().getPublisher();
+    this.publishedDate = review.getBook().getPublishedDate();
+    this.isbn = review.getBook().getIsbn();
     this.content = review.getContent();
     this.createdDate = review.getCreatedDate();
     this.updatedDate = review.getUpdatedDate();
