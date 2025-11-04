3bd1273bf959ffe345f987f35cd1e6031ac60dfe
eziquexx <waftyann@gmail.com>
2025-11-05 02:47:47 +0900

    feat(mypage) [frontend]: 리뷰 상세 모달 버튼 위치 및 스타일 수정

--- frontend/src/components/MyPageReviewModal.jsx
+++ frontend/src/components/MyPageReviewModal.jsx
@@ -119,8 +119,17 @@ const MyPageReviewModal = ({id, reviewId, fetchBookReview}) => {
           {/* 배경 */}
           <el-dialog-backdrop className="fixed inset-0 bg-gray-500/75 transition-opacity data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in"></el-dialog-backdrop>
 
-          <div tabindex="0" className="flex min-h-full items-end justify-center p-4 text-center focus:outline-none sm:items-center sm:p-0">
-            <el-dialog-panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-lg data-closed:sm:translate-y-0 data-closed:sm:scale-95">
+          <div tabindex="0" className="flex min-h-full items-center justify-center p-4 text-center focus:outline-none sm:p-0">
+            <el-dialog-panel 
+              className="
+                relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all 
+                sm:my-8 sm:w-full sm:max-w-lg 
+                w-full my-8 max-w-lg
+                data-closed:translate-y-4 data-closed:opacity-0 
+                data-enter:duration-300 data-enter:ease-out 
+                data-leave:duration-200 data-leave:ease-in 
+                data-closed:sm:translate-y-0 data-closed:sm:scale-95"
+            >
               <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                 <div className="sm:flex sm:items-start">
                   {/* <div className="mx-auto flex size-12 shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:size-10">
@@ -130,8 +139,8 @@ const MyPageReviewModal = ({id, reviewId, fetchBookReview}) => {
                   </div> */}
 
                   
-                  <div className="flex flex-col w-full mt-3 sm:mt-0 sm:text-left">
-                    <h3 id="dialog-title" className="text-base font-semibold text-gray-900 text-center sm:text-left">리뷰 상세</h3>
+                  <div className="flex flex-col w-full sm:mt-0 sm:text-left">
+                    <h3 id="dialog-title" className="text-base font-semibold text-gray-900 text-center mb-2 sm:text-left sm:mb-0">리뷰 상세</h3>
                     {/* 내용 */}
                     <div className="flex flex-col items-start mt-2">
                       {/* 도서정보 */}
@@ -143,20 +152,20 @@ const MyPageReviewModal = ({id, reviewId, fetchBookReview}) => {
                         <div className="flex flex-col w-full min-w-0 leading-6 text-[15px] ml-2 text-gray-700">
                           <div className="font-bold text-black">{data.bookTitle}</div>
                           <div className="flex flex-row text-black w-full">
-                            <span className="text-gray-500 w-1/7 mr-1">저자</span>
-                            <span className="">{data.author}</span>
+                            <span className="text-gray-500 w-2/11 sm:w-2/12">저자</span>
+                            <span className="w-8/11 sm:w-10/12">{data.author}</span>
                           </div>
                           <div className="flex flex-row text-black w-full">
-                            <span className="text-gray-500 w-1/7 mr-1">출판사</span>
-                            <span className="w-6/7">{data.publisher}</span>
+                            <span className="text-gray-500 w-2/11 sm:w-2/12">출판사</span>
+                            <span className="w-8/11 sm:w-10/12">{data.publisher}</span>
                           </div>
                           <div className="flex flex-row text-black w-full">
-                            <span className="text-gray-500 w-1/7 mr-1">출판일</span>
-                            <span className="">{data.publishedDate}</span>
+                            <span className="text-gray-500 w-2/11 sm:w-2/12">출판일</span>
+                            <span className="w-8/11 sm:w-10/12">{data.publishedDate}</span>
                           </div>
                           <div className="flex flex-row text-black w-full">
-                            <span className="text-gray-500 w-1/7 mr-1">ISBN</span>
-                            <span className="">{data.isbn}</span>
+                            <span className="text-gray-500 w-2/11 sm:w-2/12">ISBN</span>
+                            <span className="w-8/11 sm:w-10/12">{data.isbn}</span>
                           </div>
                         </div>
                       </div>
@@ -183,45 +192,54 @@ const MyPageReviewModal = ({id, reviewId, fetchBookReview}) => {
               <div 
                 className="
                   px-4 pb-7 sm:pb-5 
-                  flex-row-reverse justify-end
-                  sm:flex sm:flex-row sm:px-6"
+                  flex flex-col-reverse sm:flex-row
+                  sm:justify-between sm:px-6"
               >
-                <button
-                  type="button" 
-                  command="close" 
-                  commandfor={id} 
-                  className="
-                    inline-flex w-full justify-center rounded-md 
-                    bg-red-600 hover:bg-red-700 
-                    text-sm font-semibold text-white shadow-xs 
-                    px-3 py-2 sm:ml-2 sm:w-auto
-                    disabled:bg-gray-400
-                  "
-                  onClick={handleDeleteReview}
-                >
-                  삭제
-                </button>
-                <button 
-                  type="button" 
-                  command="close" 
-                  commandfor={id} 
-                  disabled={originReview === updateReview}
-                  className="
-                    inline-flex w-full justify-center rounded-md 
-                    bg-teal-600 hover:bg-teal-700 
-                    text-sm font-semibold text-white shadow-xs 
-                    px-3 py-2 sm:ml-2 sm:w-auto
-                    disabled:bg-gray-400
-                  "
-                  onClick={handleUpdateReview}
-                >변경</button>
-                <button 
-                  type="button" 
-                  command="close" 
-                  commandfor={id} 
-                  className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs inset-ring inset-ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto sm:ml-2"
-                  onClick={handleClose}
-                >닫기</button>
+                <div>
+                  <button
+                    type="button" 
+                    command="close" 
+                    commandfor={id} 
+                    className="
+                      order-3 sm:order-1
+                      inline-flex w-full justify-center rounded-md 
+                      bg-red-700 hover:bg-red-800 
+                      text-sm font-semibold text-white shadow-xs 
+                      px-3 py-3 sm:px-3 sm:py-2 sm:w-auto
+                      disabled:bg-gray-400
+                    "
+                    onClick={handleDeleteReview}
+                  >삭제</button>
+                </div>
+                <div>
+                  <button 
+                    type="button" 
+                    command="close" 
+                    commandfor={id} 
+                    disabled={originReview === updateReview}
+                    className="
+                      order-1 sm:order-2
+                      inline-flex w-full justify-center rounded-md 
+                      bg-teal-600 hover:bg-teal-700 
+                      text-sm font-semibold text-white shadow-xs 
+                      px-3 py-3 mb-2 sm:mb-0 sm:px-3 sm:py-2 sm:w-auto
+                      disabled:bg-gray-400
+                    "
+                    onClick={handleUpdateReview}
+                  >변경</button>
+                  <button 
+                    type="button" 
+                    command="close" 
+                    commandfor={id} 
+                    className="
+                      order-2 sm:order-3
+                      inline-flex w-full justify-center rounded-md 
+                      bg-white px-3 py-3 sm:px-3 sm:py-2 mb-5 text-sm font-semibold text-gray-900 
+                      shadow-xs inset-ring inset-ring-gray-300 
+                      hover:bg-gray-50 sm:mt-0 sm:w-auto sm:ml-2 sm:mb-0"
+                    onClick={handleClose}
+                  >닫기</button>
+                </div>
               </div>
             </el-dialog-panel>
           </div>
