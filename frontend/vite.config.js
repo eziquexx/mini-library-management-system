956e6d000900ff8237f9ca866aeac7264b2b9e70
eziquexx <waftyann@gmail.com>
2025-11-20 23:17:58 +0900

    frontend 배포 준비

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/mini-library-management-system/', // github repo 이름
   plugins: [react(), tailwindcss()],
 })
