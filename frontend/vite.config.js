d55846968b7ea0844c4d24e18d869cb6f09a79a2
eziquexx <waftyann@gmail.com>
2025-11-21 00:47:24 +0900

    vite.config.js에서 base 경로 수정

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/mini-library-management-system/', // github repo 이름
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
