f39eb080b97336dac487c2f867560a5884236696
eziquexx <waftyann@gmail.com>
2025-11-21 01:11:11 +0900

    배포시 페이지가 보이지 않아서 테스트-4 / BrowserRouter에 basename 속성 추가

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/mini-library-management-system">
       <App />
     </BrowserRouter>
   </StrictMode>,
