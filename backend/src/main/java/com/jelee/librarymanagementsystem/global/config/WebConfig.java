cbc6284e680c4a5db9cdc925237c768a2c485bf2
eziquexx <waftyann@gmail.com>
2025-11-21 21:02:30 +0900

    chor: backend 배포를 위한 준비. webconfig, application-pord.properties, Dockerfile, docker-compose.yml

--- backend/src/main/java/com/jelee/librarymanagementsystem/global/config/WebConfig.java
+++ backend/src/main/java/com/jelee/librarymanagementsystem/global/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.jelee.librarymanagementsystem.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,6 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+  @Value("${frontend.base-url}")
+  private String frontendBaseUrl;
+
+  @Value("${frontend.user-url}")
+  private String frontendUserUrl;
   
   @Override
   public void addCorsMappings(@NonNull CorsRegistry registry) {
@@ -14,7 +21,8 @@ public class WebConfig implements WebMvcConfigurer {
             .allowedOrigins(
               "http://localhost:3000", 
               "http://localhost:5173",
-              "https://eziquexx.github.io"
+              frontendBaseUrl,
+              frontendUserUrl
             ) // React 앱 주소 허용
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // 허용 HTTP 메서드
             .allowedHeaders("*")
