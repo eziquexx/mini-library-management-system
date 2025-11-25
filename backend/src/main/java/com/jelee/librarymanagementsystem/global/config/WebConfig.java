e302d1c39f575cda3a6bb0e23cc3a9ceddab9f9e
eziquexx <waftyann@gmail.com>
2025-11-25 12:54:09 +0900

    deploy: test url과 aws배포 frontend url 수정

--- backend/src/main/java/com/jelee/librarymanagementsystem/global/config/WebConfig.java
+++ backend/src/main/java/com/jelee/librarymanagementsystem/global/config/WebConfig.java
@@ -14,6 +14,9 @@ public class WebConfig implements WebMvcConfigurer {
 
   @Value("${frontend.user-url}")
   private String frontendUserUrl;
+
+  @Value("${frontend.test.base-url}")
+  private String frontendTestBaseUrl;
   
   @Override
   public void addCorsMappings(@NonNull CorsRegistry registry) {
@@ -22,7 +25,8 @@ public class WebConfig implements WebMvcConfigurer {
               "http://localhost:3000", 
               "http://localhost:5173",
               frontendBaseUrl,
-              frontendUserUrl
+              frontendUserUrl,
+              frontendTestBaseUrl
             ) // React 앱 주소 허용
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // 허용 HTTP 메서드
             .allowedHeaders("*")
