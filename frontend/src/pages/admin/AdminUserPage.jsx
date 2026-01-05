def08cf625f54a42ccd7f296607677a545cc57a4
eziquexx <waftyann@gmail.com>
2026-01-05 17:35:49 +0900

    feat(adminuserpage) [frontend]: 커스텀 테이블 추가

--- frontend/src/pages/admin/AdminUserPage.jsx
+++ frontend/src/pages/admin/AdminUserPage.jsx
@@ -1,6 +1,7 @@
 import AdminHeader from "../../components/admin/AdminHeader";
 import CustomInputSearch from "../../components/admin/CustomInputSearch";
 import CustomSelect from "../../components/admin/CustomSelect";
+import CustomTable from "../../components/admin/CustomTable";
 
 
 const AdminUserPage = () => {
@@ -13,13 +14,33 @@ const AdminUserPage = () => {
       <AdminHeader title={title} />
 
       {/* 컨텐츠 */}
-      <div className="bg-white p-4 rounded-xl shadow-[0px_0px_14px_rgba(0,0,0,0.08)]">
-        <h4 className="text-lg font-bold">회원목록</h4>
+      <div className="bg-white p-5 rounded-xl shadow-[0px_0px_14px_rgba(0,0,0,0.08)]">
+        <div>
+          <h4 className="text-lg font-bold inline-block">회원목록</h4>
+          <span className="text-sm ml-3 text-gray-400">총 50명</span>
+        </div>
         <div className="flex flex-row mt-5.5">
           <CustomSelect options={options} defaultOption={defaultOption} />
           <CustomInputSearch />
         </div>
+
+
+        <div className="mt-4">
+          <CustomTable />
+        </div>
       </div>
+
+      <style jsx>{`
+        .custom-scrollbar {
+          overflow-x: scroll; /* 수평 스크롤 활성화 */
+          -ms-overflow-style: none; /* Internet Explorer에서 스크롤바 숨김 */
+          scrollbar-width: none; /* Firefox에서 스크롤바 숨김 */
+        }
+
+        .custom-scrollbar::-webkit-scrollbar {
+          display: none; /* Webkit 기반 브라우저에서 스크롤바 숨김 */
+        }
+      `}</style>
     </div>
   );
 }
